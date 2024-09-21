f79b4f3156b3d8846137ae17113ad2cb5eb20150
mario mora <jorgemariomoramolina@gmail.com>
2024-09-21 16:09:37 -0500

    count how many vocals , there are in a string

---
+++ vowelsKata.js
@@ -0,0 +1,23 @@
+/* function vowels(str) {
+    str = str.toLowerCase();
+
+    let vowel = /[aeiou]/;
+
+    let count = str.split(vowel).length - 1;
+
+    return count
+
+} */
+
+
+//onsole.log(vowels('mire si funciono de esa manera'));
+
+
+function newVowel(str) {
+    const vowel = str.match(/[aeiou]/g);
+
+    return vowel ? vowel.length : 0;
+
+}
+
+console.log(newVowel('abracadabra'))
